b99fcf033d8a7c70cb3299c6dc80362ff1e61223
Ethan Degenhardt <ethandeg1996@gmail.com>
2020-10-12 14:36:47 -0500

    refactored js file

--- static/otakuHaven.js
+++ static/otakuHaven.js
@@ -1,46 +1,21 @@
-
-// document.querySelector('.container').addEventListener('click', addLikedGenre)
-
-
-
-// async function addLikedGenre(e) {
-//     if (e.target.dataset.liked === 'false' && e.target.tagName === 'BUTTON') {
-//         let res = await axios.post('/categories/liked', {
-//             'genre_id': e.target.dataset.id,
-//         })
-
-//         if (res.status === 201) {
-//             console.log(`You officially like ${e.target.previousElementSibling.textContent}`)
-//             e.target.dataset.liked = 'true'
-//         } else {
-//             console.log('Something went wrong')
-//         }
-//     }
-
-//     }
-
 sendGenres = document.querySelectorAll('.content button')
 
 for(let sendGenre of sendGenres){
     sendGenre.addEventListener('click', postGenre)
 }
 
-async function postGenre(e){
+function postGenre(e){
     if(e.target.dataset.liked === 'false'){
-        let res = await axios.post('/categories/liked', {
-            'genre_id': e.target.dataset.id
-        })
-
-        if(res.status === 201) {
-            console.log(`You officially like ${e.target.previousElementSibling.textContent}`)
-            e.target.dataset.liked = 'true'
-            return
-        }
+        likeGenre(e,e.target.dataset.id)
     }
    else if(e.target.dataset.liked === 'true'){
        deleteGenre(e,e.target.dataset.id)
    }
 }
+
+
+
+
 
 async function deleteGenre(e,id){
     res = await axios({
@@ -53,5 +28,19 @@ async function deleteGenre(e,id){
     if(res.status === 200){
         console.log(`You don't like ${e.target.previousElementSibling.textContent}:(`)
         e.target.dataset.liked = 'false'
+        e.target.parentElement.parentElement.dataset.liked = 'false'
+    }
+}
+
+async function likeGenre(e,id){
+    let res = await axios.post('/categories/liked', {
+        'genre_id': id
+    })
+
+    if(res.status === 201) {
+        console.log(`You officially like ${e.target.previousElementSibling.textContent}`)
+        e.target.dataset.liked = 'true'
+        e.target.parentElement.parentElement.dataset.liked = 'true'
+        return
     }
 }
